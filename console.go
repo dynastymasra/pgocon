dc44fb54579f838db3715c42a09228ba4301e0ac
Dimas Triatmaja <dynastymasra@gmail.com>
2021-04-20 12:47:56 +0700

    Add console migration for database

---
+++ console.go
@@ -0,0 +1,86 @@
+package pgocon
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"gorm.io/gorm"
+)
+
+const (
+	migrationSourcePath = "file://migration"
+	migrationFilePath   = "./migration"
+)
+
+func CreateFile(filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CreateMigrationFiles for postgres and put in migration folder
+func CreateMigrationFiles(filename string) error {
+	if len(filename) == 0 {
+		return errors.New("migration filename is not provided")
+	}
+
+	timestamp := time.Now().Unix()
+	upMigrationFilePath := fmt.Sprintf("%s/%d_%s.up.sql", migrationFilePath, timestamp, filename)
+	downMigrationFilePath := fmt.Sprintf("%s/%d_%s.down.sql", migrationFilePath, timestamp, filename)
+
+	if err := CreateFile(upMigrationFilePath); err != nil {
+		return err
+	}
+
+	if err := CreateFile(downMigrationFilePath); err != nil {
+		os.Remove(upMigrationFilePath)
+		return err
+	}
+
+	return nil
+}
+
+// Migration preparation for postgres
+func Migration(data *gorm.DB) (*migrate.Migrate, error) {
+	db, err := data.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(migrationSourcePath, "postgres", driver)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func RunMigration(migration *migrate.Migrate) error {
+	if err := migration.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
+}
+
+func RollbackMigration(migration *migrate.Migrate) error {
+	if err := migration.Steps(-1); err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/cheekybits/is v0.0.0-20150225183255-68e9c0620927 // indirect
+	github.com/golang-migrate/migrate/v4 v4.14.1
 	github.com/matryer/resync v0.0.0-20161211202428-d39c09a11215
 	gorm.io/driver/postgres v1.0.8
 	gorm.io/gorm v1.21.8
